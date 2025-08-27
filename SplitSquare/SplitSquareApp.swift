6dc6b786699cb03244543f66824dca9c90e2b23d
Dmitry Makogon <d.makogon@g.nsu.ru>
2025-08-28 00:59:44 +0700

    Initial Commit

---
+++ SplitSquare/SplitSquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitSquareApp.swift
+//  SplitSquare
+//
+//  Created by Дмитрий on 28.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct SplitSquareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SplitSquareTests/SplitSquareTests.swift
@@ -0,0 +1,17 @@
+//
+//  SplitSquareTests.swift
+//  SplitSquareTests
+//
+//  Created by Дмитрий on 28.08.2025.
+//
+
+import Testing
+@testable import SplitSquare
+
+struct SplitSquareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
